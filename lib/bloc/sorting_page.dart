34c77e5bc55c1e16f0b2ca2cacf3f9d395df3c64
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-25 11:45:03 +0530

    Create bars

--- lib/bloc/sorting_page.dart
+++ lib/bloc/sorting_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class SortingPage extends StatelessWidget {
   final List<String> sortingTypes = [
@@ -12,6 +15,38 @@ class SortingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String value = sortingTypes[0];
+
+    List<int> arr = [];
+    int height = MediaQuery.of(context).size.height.toInt();
+    int width = MediaQuery.of(context).size.width.toInt();
+
+    for (int i = 0; i < 10; ++i) {
+      int random = Random().nextInt(height - 100) + 10;
+      arr.add(random);
+    }
+
+    double division = width / arr.length;
+    double barWidth = division * 0.5;
+    double barMargin = division * 0.25;
+
+    List<Widget> children = [];
+
+    for (int i = 0; i < arr.length; ++i) {
+      Widget bar = Container(
+        height: arr[i].toDouble(),
+        width: barWidth,
+        margin: EdgeInsets.only(left: barMargin + (division * i)),
+        decoration: BoxDecoration(
+          color: Theme.of(context).accentColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+        ),
+      );
+
+      children.add(bar);
+    }
 
     return Material(
       child: Scaffold(
@@ -44,7 +79,13 @@ class SortingPage extends StatelessWidget {
           ],
         ),
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            alignment: Alignment.bottomLeft,
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: children,
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: HomePage(),
+//      home: SortingPage(),
     );
   }
 }
