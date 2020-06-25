1d3c5d823f8311b56c2482948ec49f0a24d73981
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-06-25 11:04:31 +0530

    Create sorting page ui

--- lib/bloc/sorting_page.dart
+++ lib/bloc/sorting_page.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
 class SortingPage extends StatelessWidget {
+  final List<String> sortingTypes = [
+    "Bubble Sort",
+    "Insertion Sort",
+    "Selection Sort",
+    "Merge Sort",
+    "Quick Sort"
+  ];
+
   @override
   Widget build(BuildContext context) {
+    String value = sortingTypes[0];
+
     return Material(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: DropdownButton<String>(
+            value: value,
+            onChanged: (String value) {},
+            items: sortingTypes
+                .map(
+                  (String sortingType) => DropdownMenuItem<String>(
+                    value: sortingType,
+                    child: Text(
+                      sortingType,
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: Theme.of(context).buttonColor,
+                onPressed: () {},
+                child: Text(
+                  "Sort",
+                ),
+              ),
+            )
+          ],
+        ),
         body: SafeArea(
           child: Container(),
         ),
