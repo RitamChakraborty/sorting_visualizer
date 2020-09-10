368ba7a10bfc0ded0cb5a0cd818ed64c27d26404
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-09-10 20:45:06 +0530

    Fix InputBloc for zeors and backspace

--- lib/bloc/input_bloc.dart
+++ lib/bloc/input_bloc.dart
@@ -6,16 +6,17 @@ import 'package:sortingvisualizer/bloc/events/input_event.dart';
 import 'package:sortingvisualizer/bloc/states/input_state.dart';
 
 class InputBloc extends Bloc<AbstractInputEvent, AbstractInputState> {
-  String _bars = "";
+  String _bars = "100";
 
   InputBloc() : super(InitialInputState());
 
   @override
   Stream<AbstractInputState> mapEventToState(AbstractInputEvent event) async* {
     if (event is DigitInputEvent) {
-      int tempBars = int.parse(_bars + event.digit);
-
-      if (tempBars > 1000) {
+      if (_bars.isEmpty && event.digit == "0") {
+        yield CorrectInputState(bars: "");
+      }
+      else if (int.parse(_bars + event.digit) > 1000) {
         yield ErrorInputState(
             bars: _bars, error: "Maximum number is 1000");
       } else {
@@ -24,12 +25,12 @@ class InputBloc extends Bloc<AbstractInputEvent, AbstractInputState> {
       }
     } else if (event is BackspaceInputEvent) {
       if (_bars.length > 1) {
-        _bars = _bars.substring(1);
+        _bars = _bars.substring(0, _bars.length - 1);
       } else {
         _bars = "";
       }
 
-      yield CorrectInputState(bars: "");
+      yield CorrectInputState(bars: _bars);
     } else if (event is VisualizedInputEvent) {
       if (_bars.isEmpty) {
         yield ErrorInputState(bars: _bars, error: "Please enter bar count");
